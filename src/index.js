59d303bf987c4c4bb05e5ef876ac4a8243d60b9e
Iamsindu <csitan.sindhu@gmail.com>
2022-01-22 21:07:56 +0545

    class-based components

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApprovalCard from "./ApprovalCard";
-import CommentDetail from "./CommentDetail";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          date="Yesterday at 10:00PM"
-          comment="Great post!"
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Alex"
-          date="Today at 10:00AM"
-          comment="Nice blog post."
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Jane"
-          date="Thrusday at 05:00PM"
-          comment="Good blog."
-        />
-      </ApprovalCard>
-    </div>
-  );
-};
+//----------------Functional Component
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   return <div>Hi there!</div>;
+// };
+
+//----------------Class based Component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude :</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
