d59a3932a0b70e58c2c8759267c2fcb7d658ab7d
Iamsindu <csitan.sindhu@gmail.com>
2022-01-25 11:51:46 +0545

    passing state as propos

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 
-const SeasonDisplay = () => {
+const SeasonDisplay = (props) => {
+  console.log(props.lat);
   return <div>seasons</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -13,7 +14,7 @@ class App extends React.Component {
 
   render() {
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>latitude : {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     if (!this.state.lat && this.state.errorMessage) {
       return <div>error : {this.state.errorMessage}</div>;
